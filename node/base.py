ecb0d560ce95ff2e2cf526b82a535e9ff133472b
root <ljrh@ljrh.com>
2022-08-01 18:01:50 +0800

    提交附录文件

--- node/base.py
+++ node/base.py
@@ -9,9 +9,22 @@ def step_function(x):  # 阶跃函数
     return np.array(x > 0, int)
 
 
-def softmax(a): # softmax函数
+def softmax(a):  # softmax函数
     c = np.max(a)
-    exp_a = np.exp(a-c) # 溢出策略
-    sum_exp_a= np.sum(exp_a)
-    y = exp_a/sum_exp_a
+    exp_a = np.exp(a - c)  # 溢出策略
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
     return y
+
+
+def mean_squared_error(y: np.array, t: np.array) -> object:  # 均方误差函数
+    """
+
+    :rtype: float
+    """
+    return 0.5 * np.sum((y - t) ** 2)
+
+
+def cross_entropy_error(y: np.array, t: np.array) -> object:  # 交叉熵误差
+    delta = 1e-7
+    return -np.sum(t * np.log(y+delta))
